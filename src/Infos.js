985c5663491d94ffc776f7352749e3d15927826b
icassimiro <icassimiro@hotmail.com>
2026-03-19 12:49:55 -0300

    css

--- src/Infos.js
+++ src/Infos.js
@@ -10,8 +10,8 @@ export default function Infos() {
 
                 <h2 className="infos-title">
                     <span className="l1">Gestão Documental</span>
-                    <span className="l2">Inteligente Para</span>
-                    <span className="l3">Empresas</span>
+                    <span className="l2">Inteligente</span>
+                    <span className="l3">Para Empresas</span>
                     <span className="l4">Que Valorizam</span>
                     <span className="l5">Segurança</span>
                     <span className="l6">e Eficiência</span>
